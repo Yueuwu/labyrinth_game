74be8455b65a4a40ac22d4a7bbd907cd7e82553c
Yueuwu <vladdenega1@gmail.com>
2023-03-10 20:00:50 +0700

    taken out filling func from slice

---
+++ src/utils/fillField.ts
@@ -0,0 +1,22 @@
+import {cell} from "../redux/gameSlice";
+
+export const fillField = (iterationsNum: number): cell[] => {
+    let filled: cell[] = []
+    let isCharacter = false
+    let isLoot = false
+    for (let i = 0; i < iterationsNum; i++){
+        let random = Math.floor(Math.random() * 4)
+        let status: cell['status'] = 'empty'
+        if (!random){
+            status = 'block'
+        } else if (random > 2 && !isCharacter){
+            isCharacter = true
+            status = 'character'
+        } else if (random <= 2 && !isLoot){
+            isLoot = true
+            status = 'loot'
+        }
+        filled.push({position: i, status: status})
+    }
+    return filled
+}
